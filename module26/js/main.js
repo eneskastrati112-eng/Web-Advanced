5e859a1a1f5ac038bb7a85d00403a3f78e82d843
eneskastrati112-eng <eneskastrati112@gmail.com>
2026-09-08 20:43:36 +0200

    ...

---
+++ module26/js/main.js
@@ -0,0 +1,87 @@
+console.log("Jemi ne js");
+
+// VARIABLA
+
+var x = 10;
+
+// var *ds = 10;
+
+var a = 50 + 50;
+console.log(a);
+
+var b = 20;
+var c = 30;
+var d = b+c;
+console.log(d);
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var h = 100;
+var i = 5;
+var j = h / i;
+console.log(j);
+
+var k = 50;
+var l = 4;
+var m = k % l;
+console.log(m);
+
+var n = 99;
+n++;
+console.log(n);
+
+var o = 101;
+o--;
+console.log(o);
+
+var x = 15;
+x+=5;
+console.log(x);
+
+var r = 35;
+r-=5;
+console.log(r);
+
+var t = 200;
+t/=2;
+console.log(t);
+
+var v = 5;
+var w = 5;
+console.log(v == w);
+
+var x = 6;
+var y = 7;
+console.log(x !=y);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2);
+
+var num5 = 123;
+var num6 = "123";
+console.log(num5 === num6);
+
+var firstName = "Festim";
+var lastName = "Dibrani";
+var fullName = "Festim Dibrani";
+console.log(fullName);
+
+var school = "Digital School";
+console.log(`Hello from ${school}`);
+
+var1 = 5;
+var2 = 6;
+
+console.log(var1 == 5 || var2 ==5);
+
+var age = 24;
+console.log(age >= 18 && age<= 65);
+
+var3 = 9;
+var4 = 10;
+
+console.log(!(var3==var4));
