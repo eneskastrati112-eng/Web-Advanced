2543c71fda25cff1784d0735bc5f163ded150798
eneskastrati112-eng <eneskastrati112@gmail.com>
2026-09-15 20:47:44 +0200

    ..

---
+++ Module28/Events/js/main.js
@@ -0,0 +1,31 @@
+//var button1 = document.getElementById('btn1')
+//var button2 = document.getElementsByClassName
+
+//var button2 = document.getElementById('btn2')
+//var button3 = document.getElementById('btn3')
+//var button4 = document.getElementById('btn4')
+
+
+//button1.onclick = function(){
+//   alert ("Hello from button1")
+// }
+
+//button2.onmouseover = function(){
+//   alert ("Hello from button2")
+// }
+
+//button3.onmouseleave = function(){
+//   alert ("Hello from button3")
+// }
+
+//permes queryselector
+// var firstButton = document.querySelector('button')
+
+//firstButton.onclick = function(){
+//      alert ("Hello from query selector");
+//}
+
+//var button3 = document.getElemntById(btn3);
+// button3.addEventListener('click' ,function()){
+//    alert('hello from event listener btn3')
+// })
