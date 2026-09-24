98af449764411fdb5dcf0dbcd7dbf55e26f475ab
eneskastrati112-eng <eneskastrati112@gmail.com>
2026-09-24 08:41:06 -1000

    ..

---
+++ module31/js/main.js
@@ -0,0 +1,32 @@
+//do while loop
+
+var i = 0;
+
+do{
+    i = i+1;
+    console.log(i);
+}while(i<5)
+
+console.log("______________________________________");
+
+//While loop
+
+var j = 0;
+
+while(j<5){
+    j = j+1;
+    console.log(j);
+}
+
+console.log("______________________________________");
+
+let n = 0;
+let x = 0;
+
+while(n < 3){
+    n++;
+    x+= n;
+
+    console.log(n);
+    console.log(x);
+}
